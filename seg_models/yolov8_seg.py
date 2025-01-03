d38c4905468607fafbb1d6efff23e530c46edc72
wzesk <walter@conformlab.com>
2025-01-02 20:35:32 -0500

    setup segmentation to work in repo

--- seg_models/yolov8_seg.py
+++ seg_models/yolov8_seg.py
@@ -1,15 +1,15 @@
-from google.colab.patches import cv2_imshow
 import torchvision.transforms as T
 from ultralytics import YOLO
 import numpy as np
 from PIL import Image, ImageEnhance
 from scipy import ndimage
-
+import os
 
 class YOLOV8:
-  def __init__(self,yaml,model_name='yolov8x-seg.pt'):
-    self.folder = '/yolo8_params'
+  def __init__(self,folder='/yolo8_params', model_name='yolov8x-seg.pt'):
+    self.folder = folder
     self.yaml = self.folder + "/data.yaml"
+    self.weights_path = self.folder + '/best.pt'
     self.model_name = model_name
 
   # need to retrain at higher res to match size of island
@@ -30,41 +30,52 @@ class YOLOV8:
     outDictOfClasses = {}
     return results
 
-def group_contiguous_pixels(mask_in):
-    mask_array = np.array(mask_in)
-    # Label connected components
-    labeled_array, num_features = ndimage.label(mask_array)
-    # Find the sizes of each component
-    sizes = ndimage.sum(mask_array, labeled_array, range(num_features + 1))
-    # Find the label of the largest component
-    largest_component_label = np.argmax(sizes[1:]) + 1
-    # Create a new mask with only the largest component
-    largest_component_mask = labeled_array == largest_component_label
-    largest_component_img = Image.fromarray(largest_component_mask.astype(np.uint8) * 255)
-    return largest_component_img
+  def group_contiguous_pixels(self, mask_in):
+      mask_array = np.array(mask_in)
+      # Label connected components
+      labeled_array, num_features = ndimage.label(mask_array)
+      # Find the sizes of each component
+      sizes = ndimage.sum(mask_array, labeled_array, range(num_features + 1))
+      # Find the label of the largest component
+      largest_component_label = np.argmax(sizes[1:]) + 1
+      # Create a new mask with only the largest component
+      largest_component_mask = labeled_array == largest_component_label
+      largest_component_img = Image.fromarray(largest_component_mask.astype(np.uint8) * 255)
+      return largest_component_img
 
 
-def mask_from_img(up_img, folder):
-    weights_path = self.folder + '/best.pt'
-    up_img = ImageEnhance.Contrast(up_img).enhance(2)
-
-    #run inference
-    std_results = YOLO_STD.predict(
+  def mask_from_img(self,up_img):
+      up_img = ImageEnhance.Contrast(up_img).enhance(2)
+      #run inference
+      std_results = self.predict(
         up_img,
-        YOLO(weights_path)
-    )
+        YOLO(self.weights_path)
+      )
 
-    if(std_results[0].masks != None):
-      #get mask to save
-      mask_img = T.ToPILImage()(std_results[0].masks.data[0])
+      if(std_results[0].masks != None):
+        #get mask to save
+        mask_img = T.ToPILImage()(std_results[0].masks.data[0])
 
-      #clean mask
-      single_isl = group_contiguous_pixels(mask_img)
+        #clean mask
+        single_isl = self.group_contiguous_pixels(mask_img)
 
-      #resize single_isl to match the size of img_r
-      single_isl = single_isl.resize(up_img.size)
+        #resize single_isl to match the size of img_r
+        single_isl = single_isl.resize(up_img.size)
 
-      return single_isl
-    else:
+        return single_isl
+      else:
         empty_mask = Image.new('L', up_img.size, 0)
         return empty_mask
+      
+  def mask_from_folder(self,folder):
+    masks = []
+    for root, directories, filenames in os.walk('geotools_sites'):
+      for filename in filenames:
+        if filename.endswith('_up.png'):
+            file_path = os.path.join(root,filename)
+            img = Image.open(file_path)
+            mask = self.mask_from_img(img)
+            mask_path = file_path.replace('_up.png','_mask.png')
+            mask.save(mask_path)
+            masks.append(mask_path)
+    return masks
